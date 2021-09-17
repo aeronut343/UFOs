f84baecc0385ad41f1967fc8f6d311b4ec5c506a
aeronut343 <halex211@umn.edu>
2021-09-17 12:05:21 -0500

    Added code to build table (5.2)

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,19 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select('tbody');
+
+function buildTable(data) {
+	// Clear existing data
+	tbody.html('');
+
+	// loop through each object and append the data to the table
+	data.forEach((dataRow) => {
+		// append a row
+		let row = tbody.append('tr');
+		Object.values(dataRow).forEach((val) => {
+			// append and insert data
+			let cell = row.append('td');
+			cell.text(val);
+		});
+	});
+}
